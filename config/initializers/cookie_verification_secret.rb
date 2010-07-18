4dfabc65c200e2ee7d9e14577305ac56fc8ada3d
Tejas Shah <talktejas@gmail.com>
2010-07-18 11:57:42 +0000

    Diaone design using compass, blueprint, sass and haml

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'fc8ecf9b2dcf2b0566f1c68a97fa4856457e9173ec56ceb1d2b3413e31ba10d733aec6928ba6aa8006b9dceb907859f65108bb37066f02262f41b4390f027a36';
